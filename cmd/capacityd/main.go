4666569fb83889fca93e9e3d7cba8e8a70ee6cb5
RKH Development <dev@rosskeen.house>
2019-08-08 23:16:47 -0700

    add blank capacityd main

---
+++ cmd/capacityd/main.go
@@ -0,0 +1,11 @@
+// Copyright 2019 RHK Development <dev@rosskeen.house>. All rights reserved.
+
+package main
+
+import (
+
+)
+
+func main() {
+
+}
---
+++ cmd/capacityd/main_test.go
@@ -0,0 +1,10 @@
+// Copyright 2019 RHK Development <dev@rosskeen.house>. All rights reserved.
+package main;
+
+import (
+  "testing"
+)
+
+func TestMain(m *testing.M) {
+
+}
